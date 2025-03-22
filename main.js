615013353b6c4062975009248c78dfaf1d41cffd
jo2048 <jo.sn@ikmail.com>
2025-03-22 22:34:20 +0100

    Add dynamic chart creation

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 import { Season } from "./modules/season.js";
 import { Player } from "./modules/player.js";
 import { searchPlayerByName } from "./modules/fetch_player_data.js";
-import { GameCollection } from "./modules/game.js";
-import { ChartInput, drawStackedBarChart, drawGroupedBarChart } from "./modules/display.js";
+import { ChartContainer } from "./modules/display.js";
 
 
 
-Season.fillHtmlSelect(document.getElementById("seasonSelect"))
+// Season.fillHtmlSelect(document.getElementById("seasonSelect"))
 
 function createPlayerSpan(player) {
   const span = document.createElement("span")
@@ -55,54 +54,16 @@ document.getElementById("player-pool-remove-unselected-button").addEventListener
     })
 })
 
-const statFunctions = {
-  "Games played": p => p.games_played,
-  "Goals": p => p.goals,
-  "Assists": p => p.assists,
-  "Faults": p => p.faults,
-  "Won": p => p.won,
-  "Lost": p => p.lost,
-  "Tie": p => p.tie
-}
 
 
 document.getElementById("create-chart-button").addEventListener("click", async () => {
-
   const selectedPlayersIds = Array.from(playerInPool.entries()
     .filter(([_, v]) => v.getAttribute("state") === "selected")
     .map(([k, _]) => k));
   
   if (selectedPlayersIds.length > 0 && selectedPlayersIds.length < 10) {
     const selectedPlayers = await Promise.all(selectedPlayersIds.map(async id => await Player.getPlayerById(id)))
-    const playerGames = await Promise.all(selectedPlayers.map(async p => (await GameCollection.loadPlayerGameCollection(p))))
-    const playersStats = playerGames.map(gc => [gc.player, gc.computeStats()])
-    const sortedPlayers = playersStats.sort((a1, a2) => a1[1].games_played - a2[1].games_played)
-    const wonTieLost = ["Won", "Tie", "Lost"]
-
-    const statsToDisplay = ["Games played", "Goals", "Assists", "Faults"]
-    const groupedChartInput = new ChartInput(
-      "Stats by player",
-      sortedPlayers.map(([p, _]) => p.getNameFormatted()),
-      Object.fromEntries(statsToDisplay.map(statType => [statType, sortedPlayers.map(([_, stats]) => statFunctions[statType](stats))]))      
-    )
-    
-    const stackedChartInput = new ChartInput(
-      "Won, tie, lost by player", 
-      sortedPlayers.map(([p, _]) => p.getNameFormatted()),
-      Object.fromEntries(wonTieLost.map(statType => [statType, sortedPlayers.map(([_, stats]) => statFunctions[statType](stats))]))
-    )
-
-    drawGroupedBarChart(document.getElementById("myChart"), groupedChartInput);
-    drawStackedBarChart(document.getElementById("myChart2"), stackedChartInput);
+    const chartContainer = new ChartContainer(document.getElementById("chartsDiv"), selectedPlayers);
+    await chartContainer.display()
   }
 })
-
-//   
-/*
-  title = "title"
-  x_labels = ["player 1", "player 2", "player 3", "player 4"]
-  stats = {
-    "Goals": [4,5,6,9],
-    "Assists": [2,8,3,0]
-  }
-*/
--- modules/display.js
+++ modules/display.js
@@ -1,6 +1,159 @@
-import { PlayerGame } from "./game.js";
+import { GameCollection } from "./game.js"
 
 Chart.register(ChartDataLabels);
+
+
+const statFunctions = {
+  "Games played": p => p.games_played,
+  "Goals": p => p.goals,
+  "Assists": p => p.assists,
+  "Faults": p => p.faults,
+  "Goal by match": p => p.goals_ratio,
+  "Assist by match": p => p.assists_ratio,
+  "Faults by match": p => p.faults_ratio,
+  "Won": p => p.won,
+  "Lost": p => p.lost,
+  "Tie": p => p.tie
+}
+
+
+
+class ChartContainer {
+  static nextId = 0;
+
+  constructor(parent, players) {
+    this.id = ChartContainer.#getNextId()
+    this.parent = parent
+    this.players = players
+    this.groupBy = "Player" 
+    this.playersStats = null
+    this.init()
+  }
+
+  static #getNextId() {
+    this.nextId += 1
+    return this.nextId
+  }
+
+  init() {
+    this.div = document.createElement("div")
+    this.div.classList.add("chart-container")
+    const details = document.createElement("details")
+    this.div.appendChild(details)
+    this.canvas = document.createElement("canvas")
+    this.div.appendChild(this.canvas)
+
+    const summary = document.createElement("summary")
+    summary.innerHTML = "Chart parameters"
+    details.appendChild(summary)
+
+    // Template literals
+    details.insertAdjacentHTML("beforeend", `
+      <div class="chart-container">
+        <fieldset>
+          <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+            <input type="radio" id="stats-btn-${this.id}" class="btn-check" name="btnradio" autocomplete="off" checked>
+            <label class="btn btn-outline-primary" for="stats-btn-${this.id}">Stats</label>
+            <input type="radio" id="stats-ratios-btn-${this.id}" class="btn-check" name="btnradio" autocomplete="off">
+            <label class="btn btn-outline-primary" for="stats-ratios-btn-${this.id}">Stats ratios</label>
+            <input type="radio" id="won-tie-lost-btn-${this.id}" class="btn-check" name="btnradio" autocomplete="off">
+            <label class="btn btn-outline-primary" for="won-tie-lost-btn-${this.id}">Won, tie, lost</label>
+          </div>
+          <div>
+            <span>
+              <input type="checkbox" name="games_played" id="games-played-checkbox-${this.id}" checked/>
+              <label for="games-played-checkbox-${this.id}">Total games played</label>
+            </span>        
+            <span>
+              <input type="checkbox" name="goals" id="goals-checkbox-${this.id}" checked/>
+              <label for="goals-checkbox-${this.id}">Goals</label>
+            </span>
+            <span>
+              <input type="checkbox" name="assists" id="assits-checkbox-${this.id}" checked/>
+              <label for"assits-checkbox-${this.id}"=>Assists</label>
+            </span>
+            <span>
+              <input type="checkbox" name="faults" id="faults-checkbox-${this.id}" checked />
+              <label for="faults-checkbox-${this.id}">Faults</label>
+            </span>
+          </div>
+        </fieldset>        
+    `);
+    
+    
+    this.div.querySelectorAll("input").forEach(e => e.addEventListener("click", () => this.display()));
+    this.parent.appendChild(this.div)
+  }
+
+            // <div>
+          //   <label>Select season</label>
+          //   <select name="seasons" id="seasonSelect"></select>
+          // </div>
+          // <div>
+          //   <label>Filter on</label>
+          // </div>
+          // <div>
+          //   <label>Group by</label>
+          //   <select name="groupBy" id="groupBySelect">
+          //     <option value="Season">Season</option>
+          //     <option value="Competition">Competition</option>
+          //   </select>
+          // </div>
+
+  #clearCanvas() {
+    if (this.chart)
+      this.chart.destroy()
+  }
+
+  async #getPlayerStats() {
+    if (this.playersStats == null) {
+      const playerGames = await Promise.all(this.players.map(async p => (await GameCollection.loadPlayerGameCollection(p))))
+      this.playersStats = playerGames.map(gc => [gc.player, gc.computeStats()])
+    }
+    return this.playersStats
+  }
+
+  async display() {
+    this.#clearCanvas()
+
+    var title = "Stats by player"
+    var wonTieLost = false
+    var statsToDisplay = []
+    document.getElementById(`games-played-checkbox-${this.id}`).disabled = false
+    if (document.getElementById(`stats-btn-${this.id}`).checked) {
+      statsToDisplay = []
+      if (document.getElementById(`games-played-checkbox-${this.id}`).checked)
+        statsToDisplay.push("Games played")
+      statsToDisplay.push("Goals")
+      statsToDisplay.push("Assists")
+      statsToDisplay.push("Faults")
+    } else if (document.getElementById(`stats-ratios-btn-${this.id}`).checked) {
+      document.getElementById(`games-played-checkbox-${this.id}`).checked = false
+      document.getElementById(`games-played-checkbox-${this.id}`).disabled = true
+      statsToDisplay = ["Goal by match", "Assist by match", "Faults by match"]
+    } else {
+      title = "Won, tie, lost by player"
+      wonTieLost = true
+      statsToDisplay = ["Won", "Tie", "Lost"]
+    }
+
+    const playersStats = await this.#getPlayerStats()
+    const sortedPlayers = playersStats.sort((a1, a2) => a1[1].games_played - a2[1].games_played)
+
+    const chartInput = new ChartInput(
+      title,
+      sortedPlayers.map(([p, _]) => p.getNameFormatted()),
+      Object.fromEntries(statsToDisplay.map(statType => [statType, sortedPlayers.map(([_, stats]) => statFunctions[statType](stats))]))      
+    )
+
+    if(wonTieLost)
+      this.chart = drawStackedBarChart(this.canvas, chartInput)
+    else
+      this.chart = drawGroupedBarChart(this.canvas, chartInput)
+  }
+
+}
+
 
 const plugins = {
   legend: {
@@ -20,7 +173,7 @@ const plugins = {
 };
 
 function drawStackedBarChart(ctx, chartInput) {
-  new Chart(ctx, {
+  return new Chart(ctx, {
     type: "bar",
     data: {
       labels: chartInput.x_labels,
@@ -122,7 +275,7 @@ function drawGroupedBarChart(ctx, chartInput) {
     },
   };
 
-  window.myBar = new Chart(ctx, {
+  return new Chart(ctx, {
     type: "bar",
     data: barChartData,
     options: chartOptions,
@@ -130,4 +283,4 @@ function drawGroupedBarChart(ctx, chartInput) {
   });
 }
 
-export { drawStackedBarChart, drawGroupedBarChart, ChartInput };
+export { drawStackedBarChart, drawGroupedBarChart, ChartInput, ChartContainer };
